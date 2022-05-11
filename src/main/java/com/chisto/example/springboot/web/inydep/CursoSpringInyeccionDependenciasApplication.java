5077abe3e641afa187981b6ad9499dd57b89d9c9
Don Chistorra <shadowshaoran@gmail.com>
2022-05-11 12:01:28 +0200

    Commit Inicial

---
+++ src/main/java/com/chisto/example/springboot/web/inydep/CursoSpringInyeccionDependenciasApplication.java
@@ -0,0 +1,13 @@
+package com.chisto.example.springboot.web.inydep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringInyeccionDependenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringInyeccionDependenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chisto/example/springboot/web/inydep/CursoSpringInyeccionDependenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chisto.example.springboot.web.inydep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringInyeccionDependenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
